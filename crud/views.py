872cacfbbc2ba261d365888d9a1df172f8a2ed61
todotm1227 <todo.tm1227@gmail.com>
2025-11-09 16:51:03 +0900

    20251109データの一覧を表示しよう

--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Product
+
 
 class TopView(TemplateView):
     template_name = "top.html"
+
+class ProductListView(ListView):
+    model = Product
--- myproject/urls.py
+++ myproject/urls.py
@@ -20,4 +20,5 @@ from crud import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.TopView.as_view(), name="top"),
+    path('crud/', views.ProductListView.as_view(), name="list"),
 ]

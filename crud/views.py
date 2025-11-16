1c258e274bec5e781e24e804d56445c018bc2378
todotm1227 <todo.tm1227@gmail.com>
2025-11-16 21:21:15 +0900

    20251116画像を取り扱えるようにしよう

--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Product
+from django.urls import reverse_lazy
 
 
 class TopView(TemplateView):
@@ -8,3 +10,20 @@ class TopView(TemplateView):
 
 class ProductListView(ListView):
     model = Product
+    paginate_by = 3
+
+class ProductDetailView(DetailView):
+    model = Product
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = '__all__'
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = '__all__'
+    template_name_suffix = '_update_form'
+    
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('list')
--- myproject/settings.py
+++ myproject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crud'
+    'crud',
+    'bootstrapform'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# 画像関連の設定
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media_local'
